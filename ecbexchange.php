a2ec80c42735cbbd157ae971778bf8ab5c061588
Petr Hučík <petr@getdatakick.com>
2019-07-06 11:09:53 +0200

    Release version 1.0.3

--- ecbexchange.php
+++ ecbexchange.php
@@ -48,7 +48,7 @@ class ECBExchange extends CurrencyRateModule
     {
         $this->name = 'ecbexchange';
         $this->tab = 'administration';
-        $this->version = '1.0.2';
+        $this->version = '1.0.3';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
